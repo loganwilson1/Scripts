6faa233240757ffb9289fb81dbecce8437dae797
LoganWilson1 <loganwilson@outlook.com>
2023-10-15 21:42:07 -0700

    profile updates

--- ProfileExtentions/TestExtentions.ps1
+++ ProfileExtentions/TestExtentions.ps1
@@ -1,3 +1,14 @@
 function TestWriteHost() {
     Write-Host("Hello World!")
 }
+function VsCodeSettings() {
+    code '%APPDATA%\Code\User\settings.json'
+}
+function Get-ColorOnColorWriteHostCombinations() {
+    $colors = [enum]::GetValues([System.ConsoleColor])
+    Foreach ($bgcolor in $colors)
+    {
+        Foreach ($fgcolor in $colors) { Write-Host "$fgcolor|"  -ForegroundColor $fgcolor -BackgroundColor $bgcolor -NoNewLine }
+        Write-Host " on $bgcolor"
+    }
+}
