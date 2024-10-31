c8a368135ad67de1ec2ae2f912eba1d04eeee235
LoganWilson1 <loganwilson@outlook.com>
2024-10-31 14:14:58 -0700

    Update Profile extensions readme and add Get-GitLogGraph

--- ProfileExtentions/GitFunctions.ps1
+++ ProfileExtentions/GitFunctions.ps1
@@ -4,7 +4,6 @@ Extentions for Git
 
 Import-Module posh-git # gets latest
 # Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1' # gets posh-git 0.7.3.1
-$personalGitUserName = "loganwilson1"
 
 function gitPushf() {
     $currentBranch = git rev-parse --abbrev-ref HEAD
@@ -16,3 +15,11 @@ function gitSync() {
     git pull origin $currentBranch --rebase
     gitPushf
 }
+
+function Get-GitLogGraph() {
+    param(
+    [parameter(Position = 0)][Alias("n")][int]$NumberOfCommitsToDisplay = 7
+    )
+    git log -n $NumberOfCommitsToDisplay --oneline --graph --decorate
+}
+Set-Alias -Name gg -Value Get-GitLogGraph
--- ProfileExtentions/README.md
+++ ProfileExtentions/README.md
@@ -1,5 +1,6 @@
-# To collect all the other Profile Extentions, add these lines to your profile
-``` ps1
+# Profile Extensions
+To collect all the other Profile Extentions, add these lines to your profile
+```ps1
 $profileExtentionsDir = "C:\Projects\Scripts\ProfileExtentions"
 Get-ChildItem $profileExtentionsDir\*.ps1 | ForEach-Object { . $_ }
 ```
