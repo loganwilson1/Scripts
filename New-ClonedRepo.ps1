793d2e1d7e6c2170c8003b99a2637a85b0474f67
LoganWilson1 <loganwilson@outlook.com>
2024-11-03 20:41:11 -0800

    Add New-ClonedRepo.ps1 and remote personalGitUserName references

--- New-ClonedRepo.ps1
+++ New-ClonedRepo.ps1
@@ -1,6 +1,39 @@
 <#
-Clones a repo within the $personalGitUserName (set within the PS $Profile)
+.SYNOPSIS
+Clones a GitHub repo
 
+.DESCRIPTION
+Clones a GitHub repo to the desired directory (to C:/projects if the $Directory path is not provided),
+and optionally sets up a remote targetting the forkedFromUrl (if provided).
+
+.PARAMETER RepoName
+The name of the repo to clone. (REQUIRED)
+
+.PARAMETER Directory
+The directory to clone the repo to. (Optional)
+Defaults to "C:/projects".
+
+.PARAMETER ForkedFromUrl
+URL from which the cloned repo was forked. (Optional)
+
+.EXAMPLE
+.\New-ClonedRepo.ps1 -RepoName Scripts
+Clones the user's "Scripts" repo.
+
+.\New-ClonedRepo.ps1 -RepoName Scripts -Directory C:/tools
+Clones the user's "Scripts" repo to the path 'C:/tools'
+
+.\New-ClonedRepo.ps1 -RepoName Scripts -ForkedFromUrl https://github.com/brianary/scripts
+Clones the user's "Scripts" repo and sets up a remote called 'forkedFrom' that targets https://github.com/brianary/scripts
+
+.NOTES
+File: New-ClonedRepo.ps1
+Author: Logan Wilson
+Copyright (c) 2024. All rights reserved.
+#>
+
+
+<#
 Future enhancements:
     - Make it automatically use a validate set of the list of all repos within the desired $GitUserName
     (would require Git Api, rest calls, or Git Powershell to accomplish)
@@ -8,20 +41,19 @@ Future enhancements:
 Param
 (
     [parameter(Position = 0, Mandatory = $true)][string]$RepoName,
-    [parameter(Position = 1)][string]$GitUserName = ''
+    [parameter(Position = 1)][string]$Directory = 'C:/projects',
+    [parameter(Position = 2)][string]$ForkedFromUrl = ''
 )
 
-if ($GitUserName -eq '') {
-    if ($null -eq $personalGitUserName) {
-        Write-Host "You need to add the `$personalGitUserName variable to your Microsoft.Powershell_Profile.ps1 file."
-        break
-    } else {
-        $GitUserName = $personalGitUserName
-    }
+
+$gitUserName = Get-GitConfigUserName
+Set-Location $Directory
+$cloneUrl = "https://github.com/$gitUserName/$RepoName"
+git clone $cloneUrl
+Set-Location $RepoName
+
+if ($null -ne $ForkedFromUrl) {
+    git remote add forkedFrom $ForkedFromUrl
 }
 
-
-Set-Location C:/projects
-git clone https://github.com/$personalGitUserName/$RepoName
-Set-Location $RepoName
 git remote -v
